6683d7c6700a0be6fd4fb279502df75f14a47ac4
Sofia <swl24@inf.ufpr.br>
2025-06-04 17:27:14 -0300

    added merge

--- main.c
+++ main.c
@@ -12,26 +12,76 @@ void trocar(size_t *v, size_t i, size_t j){
     return;
 } 
 
-size_t particionar(size_t *v, size_t a, size_t b){
-    size_t pivo = v[b];
-    size_t m = a;
+void min_heapfy(size_t *h, size_t i, size_t n){
+    size_t left = i * 2 ;
+    size_t right = i * 2 + 1;
+    size_t min_idx;
 
-    for(size_t i = a; i < b; i++){
-        if (v[i] <= pivo){
-            trocar(v, m, i);
-            m++;
-        }
+    if ((left <= n) && (h[left] < h[i]))
+        min_idx = left;
+    else 
+        min_idx = i;
+    if ((right <= n) && (h[right] < h[min_idx]))
+        min_idx = right;
+
+    if (min_idx != i){
+        trocar(h, i, min_idx);
+        min_heapfy(h, min_idx, n);
     }
-
-    trocar(v, m, b);
-    return m;
+    return;
 }
 
-void quicksort(size_t *v, size_t a, size_t b){
-    if (a >= b) return;
-    size_t m = particionar(v, a, b);
-    quicksort(v, a, m-1);
-    quicksort(v, m+1, b);
+void contruir(size_t *h, size_t n){
+    for(size_t i = n/2; i > 0; i--){
+        min_heapfy(h, i, n);
+    }
+
+    return;
+}
+
+void heap_sort(size_t *h, size_t n){
+    construir(h, n);
+
+    size_t i = n;
+    while(i > 1){
+        trocar(h, 1, n);
+        n--;
+        min_heapfy(h, 1, n);
+        i--;
+    }
+
+    return;
+}
+
+void copiar(size_t *final, size_t *init, size_t a, size_t b){
+    
+    for (size_t i = a; i <= b; i++)
+        final[i] = init[i]; 
+
+    return;
+}
+void merge(size_t *v, size_t a, size_t m, size_t b){
+    if (a >= b)
+        return;
+
+    size_t n = b - a + 1;
+    size_t u[n];
+    size_t i = b, j = m, k = 0;
+    size_t aux;
+    while(k < n){
+        if (j < a || (i >= m && v[i] <= v[j])){
+            aux = i;
+            i--;
+        } else {
+            aux = j;
+            j--;
+        }
+
+        u[k++] = v[aux];
+    }
+
+    copiar(u, v, a, b);
+    return;
 }
 
 int main(){
@@ -57,8 +107,7 @@ int main(){
         offset += RUN_SIZE;
 
         // ORDENAÇÃO
-        quicksort(data, 0, RUN_SIZE-1);
-
+        heap_sort(data, RUN_SIZE);    
 
         // GRAVAÇÃO
         sprintf(run_name, "run%d.txt", count_run);
@@ -71,8 +120,11 @@ int main(){
         count_run++;
     }
 
- 
     // INTERCALAÇÃO
+    for (size_t i = 1; i <= count_run; i++) {
+
+
+    }
 
     free(data);
     fclose(fp);
